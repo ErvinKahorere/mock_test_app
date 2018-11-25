3137a51f3bfd4fb7fd4610fba1c3bfbfe4c4ada5
Ervin Kahorere <ervinkahorere@gmail.com>
2018-11-25 13:52:16 +0200

    Add existing file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel on Cloudways
+                    Cheetah Mock Test
                 </div>
 
                 <div class="links">
